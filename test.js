c05d79e81ce0a31a490493d764be3eeb8ef45c47
Angel <angelf2k@gmail.com>
2020-01-30 00:45:14 -0500

    Add files via upload

---
+++ test.js
@@ -0,0 +1,9 @@
+console.time('Mesuaring time:');
+var i = 0;
+var mySrt = "This number will repeat until reach 5:";
+
+console.log(mySrt);
+for (i = 0;i<=10; i++){
+     console.log(i);    
+}
+console.timeEnd('Mesuaring time:');
